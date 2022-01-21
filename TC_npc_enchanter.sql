dac83377c6f278ca1a5b09f7e09c3fb612a185fa
anzz1 <anzz1@live.com>
2022-01-21 04:26:24 +0200

    Update TC_npc_enchanter.sql

--- TC_npc_enchanter.sql
+++ TC_npc_enchanter.sql
@@ -1,29 +1,30 @@
 -- --------------------------------------------------------------------------------------
---	ENCHANTER NPC - 601015
+--    ENCHANTER NPC - 601015
 -- --------------------------------------------------------------------------------------
 SET
-@Entry 		:= 601015,
-@Model 		:= 9353, -- Undead Necromancer
-@Name 		:= "Beauregard Boneglitter",
-@Title 		:= "Enchantments",
-@Icon 		:= "Speak",
-@GossipMenu := 0,
-@MinLevel 	:= 80,
-@MaxLevel 	:= 80,
-@Faction 	:= 35,
-@NPCFlag 	:= 1,
-@Scale		:= 1.0,
-@Rank		:= 0,
-@Type 		:= 7,
-@TypeFlags 	:= 0,
-@FlagsExtra := 2,
-@AIName		:= "PassiveAI",
-@Script 	:= "";
+@Entry         := 601015,
+@Model         := 9353, -- Undead Necromancer
+@Name          := "Beauregard Boneglitter",
+@Title         := "Enchantments",
+@Icon          := "Speak",
+@GossipMenu    := 0,
+@MinLevel      := 80,
+@MaxLevel      := 80,
+@Faction       := 35,
+@NPCFlag       := 1,
+@Scale         := 1.0,
+@Rank          := 0,
+@Type          := 7,
+@TypeFlags     := 134217728,
+@FlagsExtra    := 2,
+@AIName        := "PassiveAI",
+@HealthMod     := 1,
+@Script        := "";
 
 -- NPC
 DELETE FROM world.creature_template WHERE entry = @Entry;
-INSERT INTO world.creature_template (entry, modelid1, name, subname, IconName, gossip_menu_id, minlevel, maxlevel, faction, npcflag, scale, rank, unit_class, unit_flags, unit_flags2, type, type_flags, flags_extra, AiName, ScriptName) VALUES
-(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, @Scale, @Rank, 1, 768, 2048, @Type, @TypeFlags, @FlagsExtra, @AIName, @Script);
+INSERT INTO world.creature_template (entry, modelid1, name, subname, IconName, gossip_menu_id, minlevel, maxlevel, faction, npcflag, scale, rank, unit_class, unit_flags, unit_flags2, type, type_flags, flags_extra, AiName, MovementType, HealthModifier, ScriptName) VALUES
+(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, @Scale, @Rank, 1, 768, 2048, @Type, @TypeFlags, @FlagsExtra, @AIName, 0, @HealthMod, @Script);
 
 -- NPC EQUIPPED
 DELETE FROM world.creature_equip_template WHERE CreatureID=@Entry AND ID=1;
